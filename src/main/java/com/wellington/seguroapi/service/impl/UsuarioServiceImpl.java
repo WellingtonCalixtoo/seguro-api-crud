aad372a193c297cf2e73b688aa77f9b4c70c41d4
Wellington Calixto <wellingtoncalixtoc@gmail.com>
2026-04-09 14:32:59 -0300

    feat: criação da implementação UsuarioServiceImpl

---
+++ src/main/java/com/wellington/seguroapi/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,15 @@
+package com.wellington.seguroapi.service.impl;
+
+import com.wellington.seguroapi.dto.UsuarioDTO;
+import com.wellington.seguroapi.service.UsuarioService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Override
+    public UsuarioDTO salvar(UsuarioDTO dto) {
+        return null;
+    }
+
+}
