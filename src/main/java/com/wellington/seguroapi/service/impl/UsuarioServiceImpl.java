9e3fee270ad29cbb5e7249f0cc9a68a9b36ba1dc
Wellington Calixto <wellingtoncalixtoc@gmail.com>
2026-04-09 14:41:11 -0300

    feat: implementação do método salvar no UsuarioService

--- src/main/java/com/wellington/seguroapi/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/wellington/seguroapi/service/impl/UsuarioServiceImpl.java
@@ -1,15 +1,26 @@
 package com.wellington.seguroapi.service.impl;
 
+import com.wellington.seguroapi.domain.Usuario;
 import com.wellington.seguroapi.dto.UsuarioDTO;
+import com.wellington.seguroapi.mapper.UsuarioMapper;
+import com.wellington.seguroapi.repository.UsuarioRepository;
 import com.wellington.seguroapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioServiceImpl implements UsuarioService {
+
+    private final UsuarioRepository repository;
 
     @Override
     public UsuarioDTO salvar(UsuarioDTO dto) {
-        return null;
-    }
 
+        Usuario usuario = UsuarioMapper.toEntity(dto);
+
+        usuario = repository.save(usuario);
+
+        return UsuarioMapper.toDTO(usuario);
+    }
 }
